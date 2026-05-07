285f5f03fd6fe135b30546dcc81540db93eb52f1
shikha2209 <shikhasonikhw@gmail.com>
2026-05-07 11:28:08 +0530

    setup github pages

--- portfolio-shikha/vite.config.ts
+++ portfolio-shikha/vite.config.ts
@@ -5,6 +5,8 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    base: 'port-shikh',
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
